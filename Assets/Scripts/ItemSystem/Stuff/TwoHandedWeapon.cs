5fcfaad3c32fda9e64009514172176743b472895
hoseccan <hoseccan@gmail.com>
2020-06-25 02:03:31 +0700

    Add new player model and bunch of weapons, animations of death

--- Assets/Scripts/ItemSystem/Stuff/TwoHandedWeapon.cs
+++ Assets/Scripts/ItemSystem/Stuff/TwoHandedWeapon.cs
@@ -9,9 +9,9 @@ public class TwoHandedWeapon : Item, IItem
     public void UseItem(Invertory inventory)
     {
         inventory.weapon   = gameObject;
-        transform.position = inventory.weaponSlot.position;
         transform.SetParent(inventory.weaponSlot);
-
+        transform.localPosition = new Vector3(0, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
         Animator anim                  = inventory.GetComponent<Animator>();
         anim.runtimeAnimatorController = animOver;
     }
